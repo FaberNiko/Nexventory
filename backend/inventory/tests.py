7050ef0ce9aa90c38f54f21ee3bd51ca6dd0015e
Nikodem <nikodem.faber@interia.pl>
2026-05-06 15:08:59 +0200

    Test: add unit tests for Product.produce

--- backend/inventory/tests.py
+++ backend/inventory/tests.py
@@ -18,5 +18,32 @@ class ProductTest(TestCase):
         
         self.assertEqual(product.get_available_quantity(), 10)
 
+
+    def test_produce_raises_when_quantity_is_non_positive(self):
+        product = Product.objects.create(name="But")
+        
+        with self.assertRaises(ValueError):
+            product.produce(0)
+
+        with self.assertRaises(ValueError):
+            product.produce(-1)
+
+    def test_produce_decreases_stock(self):
+         product = Product.objects.create(name="But")
+         c1 = Component.objects.create(name="podeszwa", stock=10)
+         c2 = Component.objects.create(name="sznurówka", stock=20)
+         ProductComponent.objects.create(product=product, component=c1, quantity=1)
+         ProductComponent.objects.create(product=product, component=c2, quantity=2)
+
+         product.produce(3)
+
+         c1.refresh_from_db()
+         c2.refresh_from_db()
+         self.assertEqual(c1.stock, 7)
+         self.assertEqual(c2.stock, 14)
+        
+        
+
+
     
 # Create your tests here.
